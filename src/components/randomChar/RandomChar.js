293c89ced1f74da95d145ada05da08e72bd3b9e9
lavsora <ya.karpich@gmail.com>
2022-11-17 20:24:03 +0300

    adding method onCharLoading in RandomChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -26,6 +26,13 @@ class RandomChar extends Component {
         })
     }
 
+    onCharLoading = () => {
+        this.setState({
+            loading: true,
+            error: false
+        })
+    }
+
     onError = () => {
         this.setState({
             loading: false,
@@ -35,6 +42,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading()
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
