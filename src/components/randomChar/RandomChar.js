5e4aba651f6426e45ed3110c784616905025d8c3
lavsora <ya.karpich@gmail.com>
2022-11-17 19:01:40 +0300

    adding a mount

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,11 +7,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar()
-    }
-
     state = {
         char: {},
         loading: true,
@@ -20,19 +15,23 @@ class RandomChar extends Component {
 
     marvelService = new MarvelService();
 
+    componentDidMount() {
+        this.updateChar()
+    }
+
     onCharLoaded = (char) => {
-        this.setState({ 
+        this.setState({
             char,
             loading: false
-         })
+        })
     }
 
     onError = () => {
-        this.setState({ 
+        this.setState({
             loading: false,
             error: true
-         })
-    } 
+        })
+    }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
@@ -47,7 +46,7 @@ class RandomChar extends Component {
 
         const errorMessage = error ? <ErrorMessage /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !(loading || error) ? <View char={char}/> : null;
+        const content = !(loading || error) ? <View char={char} /> : null;
 
         return (
             <div className="randomchar">
@@ -62,7 +61,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -74,10 +73,11 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
     const { name, description, thumbnail, homepage, wiki } = char;
+    const urlImg = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={thumbnail === urlImg ? {'objectFit' : 'contain'} : {'objectFit' : 'cover'}}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
