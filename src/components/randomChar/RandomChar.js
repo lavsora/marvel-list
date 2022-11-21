4b29bc5da50a3b856a2388fe7fcd93fc8799e9be
lavsora <ya.karpich@gmail.com>
2022-11-21 12:14:31 +0300

    CharInfo update

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage'
 import MarvelService from '../../services/MarvelService';
